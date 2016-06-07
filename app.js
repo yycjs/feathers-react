5902ecc4df630fd2d39c4770573a60471d5ac10d
David Luecke <daff@neyeon.com>
2016-06-06 19:27:04 -0700

    Modify application according to demo snippets

--- app.js
+++ app.js
@@ -4,29 +4,28 @@ const socketio = require('feathers-socketio');
 const hooks = require('feathers-hooks');
 const bodyParser = require('body-parser');
 const handler = require('feathers-errors/handler');
-
 const r = require('rethinkdbdash')({
   db: 'feathers'
 });
 const rethinkdb = require('feathers-rethinkdb');
+const app = feathers();
 
-const app = feathers()
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .configure(rest())
-  .configure(socketio())
-  .configure(hooks())
-  .use('/submissions', rethinkdb({
-    Model: r,
-    name: 'submissions',
-    // Enable pagination
-    paginate: {
-      default: 10,
-      max: 25
-    }
-  }))
-  .use('/', feathers.static(__dirname))
-  .use(handler());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.configure(rest());
+app.configure(socketio());
+app.configure(hooks());
+app.use('/submissions', rethinkdb({
+  Model: r,
+  name: 'submissions',
+  // Enable pagination
+  paginate: {
+    default: 10,
+    max: 25
+  }
+}));
+app.use('/', feathers.static(__dirname));
+app.use(handler());
 
 app.service('submissions').before({
   create(hook) {
